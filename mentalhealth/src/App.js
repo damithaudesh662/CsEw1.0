3bd22516d2e354fd290fd0be443d8d2f8dfbe67f
ChamathJ <ranasingherpcgj.21@uom.lk>
2024-04-16 11:58:02 +0530

    Removed the current bootstrap implementations. Moving to tailwind

--- mentalhealth/src/App.js
+++ mentalhealth/src/App.js
@@ -1,16 +1,7 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle'
-
-import Navbar from "./components/Navbar"
-import Carousel from "./components/Carousel";
-import Featurette from './components/Featurette';
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Carousel/>
-      <Featurette/>
     </div>
   );
 }
